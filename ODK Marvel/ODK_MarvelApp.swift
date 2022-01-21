56d4385da1471d2d08107d1c7e068c36e506d116
Michael Zhang <michael@wyndot.com>
2022-01-21 10:15:12 -0800

    init xcode project

---
+++ ODK Marvel/ODK_MarvelApp.swift
@@ -0,0 +1,20 @@
+//
+//  ODK_MarvelApp.swift
+//  ODK Marvel
+//
+//  Created by wyndot on 1/21/22.
+//
+
+import SwiftUI
+
+@main
+struct ODK_MarvelApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
