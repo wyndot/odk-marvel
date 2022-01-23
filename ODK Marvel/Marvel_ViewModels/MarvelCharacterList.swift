f22f80154c9d67621bddec2e0f6a23806ab810e0
Michael Zhang <michael@wyndot.com>
2022-01-23 07:14:40 -0800

    save the favorite list locally

--- ODK Marvel/Marvel_ViewModels/MarvelCharacterList.swift
+++ ODK Marvel/Marvel_ViewModels/MarvelCharacterList.swift
@@ -20,6 +20,7 @@ final class MarvelCharacterList: ObservableObject {
     private var loadCancellable: AnyCancellable?
     
     init() {
+        try? loadFavorites()
         self.load()
     }
     
@@ -31,6 +32,7 @@ final class MarvelCharacterList: ObservableObject {
     ///     - limit: the limit parameter for the Marvel API
     ///  - NOTE:
     ///  The loaded characters will be published via characters property
+    ///
     func load(offset: Int = 0, limit: Int = 10) {
         loadCancellable?.cancel()
         
@@ -65,7 +67,7 @@ final class MarvelCharacterList: ObservableObject {
     /// - parameters:
     ///  - id: the character id
     ///
-    func addFavorite(id: Int) {
+    func addFavorite(id: Int, save: Bool = true) {
         guard characterIDs.contains(id) else {
             return
         }
@@ -84,6 +86,14 @@ final class MarvelCharacterList: ObservableObject {
         newCharacters.remove(at: idx)
         newCharacters.insert(character, at: idx)
         characters = newCharacters
+        
+        if save {
+            do {
+                try saveFavorites()
+            } catch let err {
+                print(err)
+            }
+        }
     }
     
     ///
@@ -92,7 +102,7 @@ final class MarvelCharacterList: ObservableObject {
     /// - parameters:
     ///  - id: the character id
     ///
-    func removeFavorite(id: Int) {
+    func removeFavorite(id: Int, save: Bool = true) {
         guard favoritesIDs.contains(id) else {
             return
         }
@@ -111,6 +121,44 @@ final class MarvelCharacterList: ObservableObject {
         newCharacters.remove(at: idx)
         newCharacters.insert(character, at: idx)
         characters = newCharacters
+        
+        if save {
+            do {
+                try saveFavorites()
+            } catch let err {
+                print(err)
+            }
+        }
+    }
+    
+    ///
+    /// URL to where the favorites saved
+    ///
+    private func favoritesFileURL() -> URL {
+        return FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first!.appendingPathComponent("favorites.json")
+    }
+    
+    ///
+    /// Save the favorites
+    ///
+    private func saveFavorites() throws {
+        let data = try JSONEncoder().encode(favorites)
+        try data.write(to: favoritesFileURL())
+    }
+    
+    ///
+    /// Load the saved favorites
+    ///
+    private func loadFavorites() throws {
+        let data = try Data(contentsOf: favoritesFileURL())
+        let list = try JSONDecoder().decode([MVCharacter].self, from: data)
+        append(characters: list)
+        for item in list {
+            guard let id = item.id else {
+                continue
+            }
+            addFavorite(id: id, save: false)
+        }
     }
      
     ///
